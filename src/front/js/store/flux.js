4110e94b8b45d800821b33f16dd66f1c26be85bd
Rick Patiño <77856968+Ricardo-Patino@users.noreply.github.com>
2023-07-04 13:29:59 -0600

    Update flux.js

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,25 +13,41 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			appAuth: []
 		},
 		actions: {
+			// get login/registration information
+
+			responseGoogle: response => {
+				return response;
+			},
+
+			responseFacebook: response => {
+				return response;
+			},
+
+			//push login/registration information
+			getUserIn: () => {
+				//get the store
+				const store = getStore();
+
+				//push data to store
+				store.appAuth.push(getActions().responseGoogle());
+				store.appAuth.push(getActions().responseFacebook());
+			},
+
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
+			getMessage: () => {
+				// fetching data from the backend
+				fetch(process.env.BACKEND_URL + "/api/hello")
+					.then(resp => resp.json())
+					.then(data => setStore({ message: data.message }))
+					.catch(error => console.log("Error loading message from backend", error));
 			},
 			changeColor: (index, color) => {
 				//get the store
